46d20b426db287bc6ee6974e11a6ecc592c12d98
webitya <webitya@gmail.com>
2026-08-14 15:11:37 +0530

    Aditya

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
     "Vouchiqo is a trusted offer marketplace and merchant growth platform offering 100% verified deals and analytics.",
   verification: {
     other: {
-      "verify-admitad": "f40677f5c2",
+      "verify-admitad": "aff374ca3e",
     },
   },
 };
